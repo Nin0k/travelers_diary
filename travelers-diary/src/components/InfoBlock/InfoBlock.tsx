99311fdd74e05ada9d005b2a2431b4b0b0229dcd
Aleksei <mayhembss@gmail.com>
2021-05-13 13:16:25 +0400

    added height for description

--- travelers-diary/src/components/InfoBlock/InfoBlock.tsx
+++ travelers-diary/src/components/InfoBlock/InfoBlock.tsx
@@ -1,48 +1,50 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Chip from "@material-ui/core/Chip";
-import Typography from "@material-ui/core/Typography";
-import { useInfoBlockStyles } from "./infoBlockStyles";
-import { TypeResort } from "./types";
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
+import { useInfoBlockStyles } from './infoBlockStyles';
+import { TypeResort } from './types';
 
 export const InfoBlock: React.FC<TypeResort> = ({
-    country,
-    location,
-    tags,
-    description,
+  country,
+  location,
+  tags,
+  description,
 }) => {
-    const classes = useInfoBlockStyles();
+  const classes = useInfoBlockStyles();
 
-    return (
-        <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                    className={classes.media}
-                    image="../../img.jpg"
-                    title={country}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h3" component="h2">
-                        {country}
-                    </Typography>
-                    <Typography variant="h5" component="p">
-                        {location}
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                    >
-                        {description}
-                    </Typography>
-                    {tags.map((value, index) => {
-                        return <Chip label={value} key={index} />;
-                    })}
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image='../../img.jpg'
+          title={country}
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h3' component='h2'>
+            {country}
+          </Typography>
+          <Typography variant='h5' component='p'>
+            {location}
+          </Typography>
+          <Typography
+            variant='body2'
+            color='textSecondary'
+            component='p'
+            paragraph
+            className={classes.description}
+          >
+            {description}
+          </Typography>
+          {tags.map((value, index) => {
+            return <Chip label={value} key={index} />;
+          })}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
--- travelers-diary/src/components/InfoBlock/infoBlockStyles.ts
+++ travelers-diary/src/components/InfoBlock/infoBlockStyles.ts
@@ -1,27 +1,30 @@
-import React from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 export const useInfoBlockStyles = makeStyles({
-    root: {
-        maxWidth: 345,
-    },
-    media: {
-        height: 140,
-    },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  description: {
+    height: 200,
+    overflow: 'hidden',
+  },
 });
 
 export const useContentBlockStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            marginTop: 10,
-        },
-        paper: {
-            height: 140,
-            width: 100,
-        },
-        control: {
-            padding: theme.spacing(2),
-        },
-    })
+  createStyles({
+    root: {
+      flexGrow: 1,
+      marginTop: 10,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  })
 );
