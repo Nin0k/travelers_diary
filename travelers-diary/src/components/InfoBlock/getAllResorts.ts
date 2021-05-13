e1fbef88558d81931e58548c6561b36e5bfe4fb7
Aleksei <mayhembss@gmail.com>
2021-05-13 15:08:23 +0400

    added proxy

--- travelers-diary/src/components/InfoBlock/getAllResorts.ts
+++ travelers-diary/src/components/InfoBlock/getAllResorts.ts
@@ -1,15 +1,16 @@
-import { TypeResort } from "./types";
+import { TypeResort } from './types';
 
-const HOST = "http://localhost:5000";
-const getUrl = `${HOST}/api/getinfo/all`;
+const url = `/api/getinfo/all`;
 
 export const getAllResorts = () => {
-    return new Promise<TypeResort[]>((resolve, reject) => {
-        fetch(getUrl).then((response) => {
-            return resolve(response.json());
-        },
-        (error) => {
-          return reject(error);
-        })
-    });
+  return new Promise<TypeResort[]>((resolve, reject) => {
+    fetch(url).then(
+      (response) => {
+        return resolve(response.json());
+      },
+      (error) => {
+        return reject(error);
+      }
+    );
+  });
 };
